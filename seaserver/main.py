b3bd583230dff847033d577adbb7d45c1e36d98a
Joseph Thurlow <joseph.thurlow7d0@gmail.com>
2024-11-14 22:57:40 +1000

    updated host

--- seaserver/main.py
+++ seaserver/main.py
@@ -3,6 +3,6 @@ import os
 from seaserver.app import app
 
 if __name__ == "__main__":    
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 5000))
 
-    app.run(threaded=True, debug=True, host="0.0.0.0", port=port)
+    app.run(threaded=True, debug=True, host="localhost", port=port)
